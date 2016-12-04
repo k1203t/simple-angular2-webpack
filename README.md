db859574fa500aeb72bf26200c8c2b0bf734ded3
寺田浩介 <KosukeTerada@macdev.local>
2016-12-04 23:34:10 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# SimpleAngular2Webpack
+# simple-angular2-webpack
 A simple angular2 project using webpack
+
+### Quick start
+
+```bash
+# clone our repo
+$ git clone https://github.com/kostera/simple-angular2-webpack.git
+
+# change directory to your app
+$ cd simple-angular2-webpack
+
+# install the dependencies with npm
+$ npm install
+
+# start the server
+$ npm run serve
+```
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var webpack = require('webpack');
+
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+  output: {
+    path: './dist',
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].chunk.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          'awesome-typescript-loader',
+          'angular2-template-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader',
+        exclude: ['./src/index.html']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['to-string-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+    ]
+  },
+  plugins: [
+    new CommonsChunkPlugin({
+      name: ['vendor', 'polyfills']
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunksSortMode: 'dependency'
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets', to: 'assets' }
+    ]),
+  ],
+  resolve: {
+    extensions: ['', '.ts', '.js', '.json', '.css', '.scss', '.html'],
+  }
+}
